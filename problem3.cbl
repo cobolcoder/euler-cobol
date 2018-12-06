37b69d399cb931ab45b437ef51c17faab8abf9cd
Cobol Coder <>
2018-12-06 18:02:37 +0000

    Update problem3.cbl

--- problem3.cbl
+++ problem3.cbl
@@ -14,34 +14,33 @@
            01 WS-MAX-FACTOR           PIC 9(12) VALUE 2.
            01 WS-RMNDR                PIC 9(12) VALUE 2.
            01 WS-RSLT                 PIC 9(12) VALUE 0.
+           01 WS-RSLT-FRMT            PIC Z(12) VALUE 0.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            DISPLAY "WORKING ON PROJECT EULER PROBLEM 3".
-           DISPLAY "LARGEST PRIME FACTOR OF 600851475143".
+           DISPLAY "LARGEST PRIME FACTOR FOR 600851475143".
 
-           DIVIDE WS-TARGET-VALUE BY WS-PRIME-FACTOR
-               GIVING WS-RSLT REMAINDER WS-RMNDR.
-
-           PERFORM UNTIL WS-RSLT = 1
-
-               IF WS-RMNDR = 0
-                   
-                   DISPLAY "FOUND FACTOR:" WS-PRIME-FACTOR
-                   IF WS-PRIME-FACTOR > WS-MAX-FACTOR
-                       MOVE WS-PRIME-FACTOR TO WS-MAX-FACTOR
-                   END-IF
-
-                   MOVE WS-PRIME-FACTOR TO WS-MAX-FACTOR
-                   MOVE 1 TO WS-PRIME-FACTOR
-                   MOVE WS-RSLT TO WS-TARGET-VALUE
-               END-IF
-               
-               ADD 1 TO WS-PRIME-FACTOR GIVING WS-PRIME-FACTOR 
+           PERFORM UNTIL WS-RSLT = 1 AND WS-RMNDR = 0
 
                DIVIDE WS-TARGET-VALUE BY WS-PRIME-FACTOR
                    GIVING WS-RSLT REMAINDER WS-RMNDR
 
+               IF WS-RMNDR = 0
+
+                   IF WS-PRIME-FACTOR > WS-MAX-FACTOR
+                       MOVE WS-PRIME-FACTOR TO WS-MAX-FACTOR
+                   END-IF
+
+                   MOVE 1 TO WS-PRIME-FACTOR
+                   MOVE WS-RSLT TO WS-TARGET-VALUE
+               END-IF
+
+               ADD 1 TO WS-PRIME-FACTOR GIVING WS-PRIME-FACTOR
+
            END-PERFORM.
+
+           MOVE WS-MAX-FACTOR TO WS-RSLT-FRMT.
+           DISPLAY "LARGEST FACTOR: " WS-RSLT-FRMT.
 
             STOP RUN.
        END PROGRAM PROBLEM-3.
